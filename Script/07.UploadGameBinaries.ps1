ed63fd0c4f64ba9e3268441cf239f80323bb2687
Hendra Darwintha <hendra@accelbyte.net>
2022-05-26 08:51:02 +0700

    fix(buildsystem): update bitbucket pipeline to only run on master branch only, adding custom pipeline

--- Script/07.UploadGameBinaries.ps1
+++ Script/07.UploadGameBinaries.ps1
@@ -7,7 +7,5 @@ $EntryPoint = "LyraGame.exe"
 
 
 Write-Output "---Step 7: Uploading Game Binaries for Game client---"
-# Execute the command
-Invoke-Expression "${BlackBoxCLIPath} build upload-binaries --entry-point ${EntryPoint} --namespace ${Namespace} --apikey ${APIKey} --game-project=${GameProjectPath} --game-engine=${UE4GameEnginePath} --game-archive=${GameClientPath}"
-
+Invoke-Expression "${BlackBoxCLIPath} build upload-binaries --entry-point ${EntryPoint} --namespace ${Namespace} --apikey ${APIKey} --game-project=${GameProjectPath} --game-engine=${UE4GameEnginePath} --game-archive=${GameClientPath} --launch-argument=-nosteam"
 Write-Output "---Step 7: Uploading Game Binaries for Game client---DONE"
--- Script/11.BuildLinuxGameServer.ps1
+++ Script/11.BuildLinuxGameServer.ps1
@@ -4,10 +4,6 @@
 Write-Output "---Step 11: Building Game Server (Linux) Executable---"
 
 $GameServerPathAbsolute = Resolve-Path -Path $OutputLinuxGameServerFolder
-# Write-Output "DELETING ${GameServerPathAbsolute}..."
-# Get-ChildItem -Path "${GameServerPathAbsolute}" *.* -Recurse | foreach { $_.Delete()}
-# Write-Output "DELETING ${GameServerPathAbsolute}...DONE"
-# Remove-Item "${GameServerPathAbsolute}\*" -Recurse -Force
 
 # Execute the command
 Invoke-Expression "${UE4BatchFile} BuildCookRun -project=${GameProjectFile} -noP4 -platform=Linux -clientconfig=Development -cook -serverconfig=Development -server -serverplatform=Linux -noclient -build -stage -pak -archive -crashreporter -archivedirectory=${OutputLinuxGameServerFolder}"
