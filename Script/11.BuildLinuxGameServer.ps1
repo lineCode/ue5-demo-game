ab91d3c57fa145e9defdb6bbdf75d3a32c6cc09e
Hendra Darwintha <hendra@accelbyte.net>
2022-05-26 16:59:41 +0700

    fix(buildsystem): fix error script on the upload game server binary, add armada uploader exe

--- Script/11.BuildLinuxGameServer.ps1
+++ Script/11.BuildLinuxGameServer.ps1
@@ -3,4 +3,8 @@
 
 Write-Output "---Step 11: Building Game Server (Linux) Executable---"
 Invoke-Expression "${UE4BatchFile} BuildCookRun -project=${GameProjectFile} -noP4 -platform=Linux -clientconfig=Development -cook -serverconfig=Development -server -Target=LyraGame -serverplatform=Linux -noclient -build -stage -pak -archive -crashreporter -archivedirectory=${OutputLinuxGameServerFolder}"
+
+Write-Output "Viewing the output folder content"
+Invoke-Expression "tree ${OutputLinuxGameServerFolder} /F"
+
 Write-Output "---Step 11: Building Game Server (Linux) Executable---DONE"
