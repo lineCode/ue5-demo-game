553ddf66e9f70413ffe78bac440256a80ba473d6
Hendra Darwintha <hendra@accelbyte.net>
2022-05-26 11:02:28 +0700

    fix (buildsystem): fix error script on building game package for linux

--- Script/11.BuildLinuxGameServer.ps1
+++ Script/11.BuildLinuxGameServer.ps1
@@ -2,10 +2,5 @@
 . $PSScriptRoot\00.Configuration.ps1
 
 Write-Output "---Step 11: Building Game Server (Linux) Executable---"
-
-$GameServerPathAbsolute = Resolve-Path -Path $OutputLinuxGameServerFolder
-
-# Execute the command
-Invoke-Expression "${UE4BatchFile} BuildCookRun -project=${GameProjectFile} -noP4 -platform=Linux -clientconfig=Development -cook -serverconfig=Development -server -serverplatform=Linux -noclient -build -stage -pak -archive -crashreporter -archivedirectory=${OutputLinuxGameServerFolder}"
-
+Invoke-Expression "${UE4BatchFile} BuildCookRun -project=${GameProjectFile} -noP4 -platform=Linux -clientconfig=Development -cook -serverconfig=Development -server -Target=LyraGame -serverplatform=Linux -noclient -build -stage -pak -archive -crashreporter -archivedirectory=${OutputLinuxGameServerFolder}"
 Write-Output "---Step 11: Building Game Server (Linux) Executable---DONE"
